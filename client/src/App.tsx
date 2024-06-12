af967f79551a01bf0b7470dd91524fe583ebd873
ihide14 <juandiman26@gmal.com>
2024-06-12 18:26:34 -0500

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import { Login } from "./pages/Login"
+import { Dashboard } from "./pages/DashBoard"
+
+function App() {
+
+  return (
+    <Routes>
+      <Route index element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { useForm } from 'react-hook-form'
+
+export const Login = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+
+    return (
+        <>
+        </>
+    )
+}
